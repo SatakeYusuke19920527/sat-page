b2a65502acaf623fa7fe61e6820a4ece5977a563
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-07-09 00:01:49 +0900

    Updates

---
+++ precache-manifest.9949f0f630ff84c32ed11c9ec43a8486.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88524dfdaa8c050016fb07007fadcbac",
+    "url": "/SatakeYusuke19920527/sat-page/index.html"
+  },
+  {
+    "revision": "61e66e1e45e6fd20e8a6",
+    "url": "/SatakeYusuke19920527/sat-page/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "be5528b3650d9593d669",
+    "url": "/SatakeYusuke19920527/sat-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61e66e1e45e6fd20e8a6",
+    "url": "/SatakeYusuke19920527/sat-page/static/js/2.023f2278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SatakeYusuke19920527/sat-page/static/js/2.023f2278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5528b3650d9593d669",
+    "url": "/SatakeYusuke19920527/sat-page/static/js/main.baed682f.chunk.js"
+  },
+  {
+    "revision": "1e224ba92157eeb83a7b",
+    "url": "/SatakeYusuke19920527/sat-page/static/js/runtime-main.bd2c0837.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SatakeYusuke19920527/sat-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
